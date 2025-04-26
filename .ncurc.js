61e786f0ddc5824cc685b46d7aa64b111d627f6c
notmedia <vasyukov.alexey.v@gmail.com>
2025-04-26 13:14:16 +0300

    chore: update deps

--- .ncurc.js
+++ .ncurc.js
@@ -1,5 +1,9 @@
 /** @type {import('npm-check-updates').RunOptions} */
 module.exports = {
   dep: ['prod', 'dev', 'optional', 'peer'],
-  reject: [],
+  reject: [
+    // wallaby errors on vitest 3.1.2
+    '@vitest/coverage-v8',
+    'vitest'
+  ],
 };
--- packages/logger-pretty/.ncurc.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('npm-check-updates').RunOptions} */
-module.exports = {
-  dep: ['prod', 'dev', 'optional', 'peer'],
-  reject: [],
-};
